3c24e6301fa50e2ebd7ef7eeb8789a80103313c4
Fry98 <filip-toman@seznam.cz>
2018-11-12 00:29:38 +0100

    Reworked textbox + Added images to chat

--- js/chat.js
+++ js/chat.js
@@ -8,3 +8,15 @@ $('#chans li').click(function(){
     currChan = newIndex;
   }
 });
+
+$('#msg-box textarea').focus(()=>{
+  $('#msg-box').css('border-color', 'rgb(28, 126, 192)');
+});
+
+$('#msg-box textarea').blur(()=>{
+  $('#msg-box').css('border-color', '');
+});
+
+$('.msg img').click(()=>{
+  alert('clicked img')
+});
